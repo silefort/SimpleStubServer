e4427220399243bdb5f158263846dd290a1787da
Simon Lefort <simon.lefort@gmail.com>
2019-09-15 16:43:25 +0200

    added a yeserver

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
 from flask import Flask
 from flask import make_response
-
-def build_response(body, statusCode, headers,responseTime):
-    resp = make_response(body, statusCode)
-    resp.headers = headers
-    return resp
+import yesserver
+import requests
 
 if __name__ == "__main__":
-    app = Flask(__name__)
 
-    # make a "check" route to test the web server
-    @app.route('/check')
-    def check():
-        return "OK", 200
+    app = yesserver.YesServer()
+    app.start()
 
-    # All other routes should end up in a 404
-    # We catch it to provide custom responses
-    @app.errorhandler(404)
-    def page_not_found(error):
-        headers = {}
-        headers['X-entete'] = 'entete'
-        headers['X-entete2'] = 'entete3'
-        return build_response("ma rep",304, headers)
-    
-    app.run(host="0.0.0.0")
+    r = requests.get('http://localhost:5000/check')
+    print "check:"
+    print r.text
+
+    r = requests.post('http://localhost:5000/bla')
+    print "bla:"
+    print r.status_code
+    print r.text
+
+    app.stop()
---
+++ yesserver.py
@@ -0,0 +1,36 @@
+import flask
+import time
+import multiprocessing
+import logging
+
+class YesServer:
+    def __init__(self, debug=False, port=5000):
+        self.server = None
+        self.port = port;
+        self.manager = multiprocessing.Manager()
+
+    def server_thread(self):
+        app = flask.Flask(__name__)
+
+        @app.errorhandler(404)
+        def page_not_found(error):
+            print self.port
+            return "OK", 200
+        
+        app.run(host="0.0.0.0", port=self.port)
+
+    def start(self):
+        """
+        Start the server (async)
+        """
+        self.server = multiprocessing.Process(target=self.server_thread)
+        self.server.start()
+        time.sleep(0.5) # Let the server start, just in case...
+
+    def stop(self):
+        """
+        Stop the server
+        """
+        self.manager.shutdown()
+        self.server.terminate()
+        self.server.join()
